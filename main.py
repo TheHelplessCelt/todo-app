9550e60fc659120b0cf92d088577e9333d4697d2
SamT <example@example.com>
2020-02-17 10:59:38 +0000

    added blocked tags and commented code

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import os
 
 from task_store import TaskList
 
-
+# Initialize the task list
 tl = TaskList()
 
 
 def list_tasks():
+    """displays currently open tasks
+    """
     for k, v in tl.task_list.items():
         print("\033[1m" + k + "\033[0m" + ": " + v)
 
 
 def menu():
+    """command menu, run multiple times, messy but works
+    """
+
+    # accept user input for command
     cmd = input("\033[1m\033[0;36m>\033[0m ")
+
+    # command parsing
     if cmd[0] == "a" and cmd[1] == " ":
         tl.add_task(cmd[2:])
 
@@ -25,21 +33,29 @@ def menu():
     elif cmd[0] == "d" and cmd[1] == " ":
         tl.track_doing_task(cmd[2:])
 
+    elif cmd[0] == "b" and cmd[1] == " ":
+        tl.track_blocked_task(cmd[2:])
+
     elif cmd[0] == "z" and cmd[1] == " ":
         tl.remove_done(cmd[2:])
 
     elif cmd[0] == "x" and cmd[1] == " ":
         tl.remove_doing(cmd[2:])
 
+    elif cmd[0] == "v" and cmd[1] == " ":
+        tl.remove_blocked(cmd[2:])
+
     elif cmd == "e":
         os.system("clear")
         exit()
 
+    # pass used to catch unwanted input
     else:
         pass
 
 
 while True:
+    # clear the screen every run to avoid ncurses
     os.system("clear")
     print("\033[0;35m====================\033[0;34mTODO LIST\033[0m\033[0;35m====================\033[0m")
     list_tasks()
--- task_store.py
+++ task_store.py
@@ -6,6 +6,7 @@ import json
 
 
 def get_time():
+    # TODO actually use this
     return calendar.timegm(time.gmtime())
 
 
@@ -16,6 +17,11 @@ class TaskList:
         atexit.register(self.save_tasks)
 
     def add_task(self, row):
+        """adds the description of a task to task_list dict
+
+        Arguments:
+            row {string} -- the description of a task
+        """
         if len(self.task_list) == 0:
             self.task_list["0"] = row
         else:
@@ -24,21 +30,38 @@ class TaskList:
             self.task_list[str(max(keys) + 1)] = row
 
     def remove_task(self, id):
+        """removes a task at the specified index
+
+        Arguments:
+            id {int} -- the index to remove
+        """
         try:
             del self.task_list[id]
         except KeyError:
             print("not a valid id")
 
     def save_tasks(self):
+        """writes the task list to a json file upon close
+        """
+
         with open("task_list.json", "w+") as js_file:
             json.dump(self.task_list, js_file)
 
     def read_tasks(self):
+        """read the task list json at launch
+        """
+
         if os.path.exists("task_list.json"):
             with open("task_list.json", "r") as js_file:
                 self.task_list = dict(json.load(js_file))
 
     def remove_done(self, id):
+        """removes the done tag from a specified tag
+
+        Arguments:
+            id {int} -- the task to remove the tag from
+        """
+
         try:
             row = self.task_list[id]
         except KeyError:
@@ -46,7 +69,27 @@ class TaskList:
 
         self.task_list[id] = row.replace("\033[0;32m ✓\033[0m", "")
 
+    def remove_blocked(self, id):
+        """removes the blocked tag from a specified tag
+
+        Arguments:
+            id {int} -- the task to remove the tag from
+        """
+
+        try:
+            row = self.task_list[id]
+        except KeyError:
+            print("not a valid id")
+
+        self.task_list[id] = row.replace("\033[0;31m ✗\033[0m", "")
+
     def remove_doing(self, id):
+        """removes the doing tag from a specified tag
+
+        Arguments:
+            id {int} -- the task to remove the tag from
+        """
+
         try:
             row = self.task_list[id]
         except KeyError:
@@ -54,19 +97,49 @@ class TaskList:
         self.task_list[id] = row.replace("\033[1;33m ●\033[0m", "")
 
     def track_done_task(self, id):
+        """adds the unicode character for a completed task, removes the blocked or doing tags
+
+        Arguments:
+            id {int} -- the task to add the tag to
+        """
         try:
             if "●" in self.task_list[id]:
                 self.task_list[id] = self.task_list[id].replace("\033[1;33m ●\033[0m", "\033[0;32m ✓\033[0m")
+            elif "✗" in self.task_list[id]:
+                self.task_list[id] = self.task_list[id].replace("\033[0;31m ✗\033[0m", "\033[0;32m ✓\033[0m")
             else:
                 self.task_list[id] += "\033[0;32m ✓\033[0m"
         except KeyError:
             print("not a valid id")
 
     def track_doing_task(self, id):
+        """adds the unicode character for an in progress task, removes the blocked or done tags
+
+        Arguments:
+            id {int} -- the task to add the tag to
+        """
         try:
             if "✓" in self.task_list[id]:
                 self.task_list[id] = self.task_list[id].replace("\033[0;32m ✓\033[0m", "\033[1;33m ●\033[0m")
+            elif "✗" in self.task_list[id]:
+                self.task_list[id] = self.task_list[id].replace("\033[0;31m ✗\033[0m", "\033[1;33m ●\033[0m")
             else:
                 self.task_list[id] += "\033[1;33m ●\033[0m"
+        except KeyError:
+            print("not a valid id")
+
+    def track_blocked_task(self, id):
+        """adds the unicode character for a blocked task, removes the doing or done tags
+
+        Arguments:
+            id {int} -- the task to add the tag to
+        """
+        try:
+            if "✓" in self.task_list[id]:
+                self.task_list[id] = self.task_list[id].replace("\033[0;32m ✓\033[0m", "\033[0;31m ✗\033[0m")
+            elif "●" in self.task_list[id]:
+                self.task_list[id] = self.task_list[id].replace("\033[1;33m ●\033[0m", "\033[0;31m ✗\033[0m")
+            else:
+                self.task_list[id] += "\033[0;31m ✗\033[0m"
         except KeyError:
             print("not a valid id")
